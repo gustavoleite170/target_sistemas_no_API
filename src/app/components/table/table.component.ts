1ff8e11b02ce4989adfedd02ef97de7ecc4a5446
Gustavo Leite <gustavo.leite170@gmail.com>
2022-04-04 23:17:36 -0300

    created json to contain data

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -15,16 +15,16 @@ export class User {
 }
 
 const ELEMENT_DATA: Person[] = [
-  {position: 1, name: 'Maria Dolores', phoneNumber: "(27) 3496-4172"},
-  {position: 2, name: 'Reginaldo Soares', phoneNumber: "(84) 2353-7216"},
-  {position: 3, name: 'Francisco Silva', phoneNumber: "(15) 3797-4431"},
-  {position: 4, name: 'Raimundo Reis', phoneNumber: "(43) 2312-1248"},
-  {position: 5, name: 'Sérgio Fagundes', phoneNumber: "(34) 2878-5688"},
-  {position: 6, name: 'Hanna Spector', phoneNumber: "(54) 3700-1787"},
-  {position: 7, name: 'Manoel Souza', phoneNumber: "(69) 2858-7155"},
-  {position: 8, name: 'Felipe Mendez', phoneNumber: "(61) 3218-2586"},
-  {position: 9, name: 'Ricardo Patriota', phoneNumber: "(38) 2336-7087"},
-  {position: 10, name: 'José Ferreira Leite', phoneNumber: "(94) 2347-4826"},
+  {position: 1, name: "Maria Dolores", phoneNumber: "(27) 3496-4172"},
+  {position: 2, name: "Reginaldo Soares", phoneNumber: "(84) 2353-7216"},
+  {position: 3, name: "Francisco Silva", phoneNumber: "(15) 3797-4431"},
+  {position: 4, name: "Raimundo Reis", phoneNumber: "(43) 2312-1248"},
+  {position: 5, name: "Sérgio Fagundes", phoneNumber: "(34) 2878-5688"},
+  {position: 6, name: "Hanna Spector", phoneNumber: "(54) 3700-1787"},
+  {position: 7, name: "Manoel Souza", phoneNumber: "(69) 2858-7155"},
+  {position: 8, name: "Felipe Mendez", phoneNumber: "(61) 3218-2586"},
+  {position: 9, name: "Ricardo Patriota", phoneNumber: "(38) 2336-7087"},
+  {position: 10, name: "José Ferreira Leite", phoneNumber: "(94) 2347-4826"},
 ];
 
 
@@ -54,15 +54,13 @@ export class TableComponent implements AfterViewInit {
     
   }
 
+  /* For avoiding trouble of reponse time, used afterviewinit with liveAnnouncer. Live announcer ensures the display of the sorted table*/
+
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
   }
 
   announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
     if (sortState.direction) {
       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
@@ -70,21 +68,27 @@ export class TableComponent implements AfterViewInit {
     }
   }
 
-  columnsToDisplay: string[] = ["userName", "age"];
-    user = new User();
-    userData: User[]= [
-        { userName: "Wacco", age: 12 },
-        { userName: "Wacca", age: 13 },
-        { userName: "Waccu", age: 14 }
-    ];
-    myDataArray = new MatTableDataSource(this.userData);
+/* Trying to make the adder */
 
-    addName() {
-       let newUser : User = {
-          userName : this.user.userName,
-          age : this.user.age
-       }
-       this.userData.push(newUser);
-       this.myDataArray.data = this.userData;
-    }
+
+  columnsToDisplay: string[] = ["userName", "age"];
+  user = new User();
+  userData: User[]= [
+      { userName: "Wacco", age: 12 },
+      { userName: "Wacca", age: 13 },
+      { userName: "Waccu", age: 14 }
+  ];
+  myDataArray = new MatTableDataSource(this.userData);
+
+  addName() {
+      let newUser : User = {
+        userName: this.user.userName,
+        age: this.user.age
+      }
+      this.userData.push(newUser);
+      this.myDataArray.data = this.userData;
+      console.log(this.user)
+      console.log(this.userData)
+  }
+    
 }
