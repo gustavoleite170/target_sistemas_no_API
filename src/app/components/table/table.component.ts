1b9f157d0bc3299ba0291d8b12bb559f1ddbae49
Gustavo Leite <gustavo.leite170@gmail.com>
2022-04-06 00:54:02 -0300

    added method to find the element to be deleted. just a fill lines to make tests

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -2,8 +2,9 @@ import { TableService } from './table.service';
 import { Component, ViewChild, OnInit} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort} from '@angular/material/sort';
-import {MatPaginator} from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { People } from './people.model';
+import { ActivatedRoute } from "@angular/router";
 
 
 @Component({
@@ -17,17 +18,25 @@ export class TableComponent implements OnInit {
   displayedColumns: string[] = ['name', 'phoneNumber', 'action'];
   dataSource: any;
   people: People[];
+  delPerson: People;
 
-  constructor(private tableService: TableService) {}
+  constructor(private tableService: TableService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     /* Consuming API, doing filtering and pagination  */
     this.tableService.read().subscribe(response => {
       this.dataSource = new MatTableDataSource(response);
-      
+
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator
     })
+
+    /* Finding the element in order to delete*/
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.tableService.readById(id).subscribe((response) => {
+      this.delPerson = response;
+    });
+
   }
 
   /* Sort and pagination Implementation */
